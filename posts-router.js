19bb64f043437b19ac6dd7a20b2779c69f1fe2d9
Lee Damien <notesong.git@outlook.com>
2020-03-14 19:56:59 -0400

    Basic routing set up with get for posts

---
+++ posts-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Posts = require('./data/db.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => res.status(500).json({ message: 'Error' }));
+})
+
+module.exports = router;
